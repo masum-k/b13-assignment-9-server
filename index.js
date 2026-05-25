a233b68a00c16d910063f03ee1e7a5bb287087e8
masum-k <maasumkamal@gmail.com>
2026-05-25 18:07:47 +0600

    getting data from  mongodb server

--- index.js
+++ index.js
@@ -1,6 +1,51 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
 const app = express();
-const port = 3001;
+
+dotenv.config();
+app.use(cors())
+
+const port = process.env.PORT || 8000;
+
+
+
+
+
+
+
+
+const uri = "mongodb+srv://medi_queue_tutor:pg6rtpBscRtuEjFO@cluster0.ret3q0v.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+  
+    await client.connect();
+   
+    const db = client.db("mediqueue")
+    const tutorsCollection = db.collection("tutors")
+
+
+
+
+
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close(); 
+  }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
